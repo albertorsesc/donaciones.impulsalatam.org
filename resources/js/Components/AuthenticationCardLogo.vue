c553e803a1a5b957da51d504ad8a4d6a55a2ff6e
Alberto Rosas <alberto.rsesc@protonmail.com>
2025-04-01 09:11:53 -0700

    Jetstream installed

---
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Link :href="'/'">
+        <img src="/img/logo.png" alt="Logo ImpulsaLATAM" width="120">
+    </Link>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing.welcome');
 });
 
 Route::get('/acerca-de', function () {
-    return view('about');
+    return view('landing.about');
 })->name('about');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
